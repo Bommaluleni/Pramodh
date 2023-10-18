01e426ba5b513ef7edd65dc27c32feeaa734b411
Pramodh Kumar <bommalulenipramodh@gmail.com>
2023-10-19 00:14:30 +0530

    updated Sample Class

---
+++ Sample.java
@@ -0,0 +1,21 @@
+package main;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "C://Automation Java//Selenium Jars//chromedriver-win64//chromedriver-win64//chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.flipkart.com/");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.findElement(By.xpath("//span[@role='button']")).click();
+	}
+
+}
